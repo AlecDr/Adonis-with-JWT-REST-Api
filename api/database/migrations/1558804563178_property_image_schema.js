cda41b9c5361561c01355b63b3b1525139fb7201
AlecDr <alecsander_rs@hotmail.com>
2019-05-25 14:18:05 -0300

    adding eslint configuration and adding the properties and property image model and migrations

---
+++ api/database/migrations/1558804563178_property_image_schema.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class PropertyImageSchema extends Schema {
+  up() {
+    this.create("property_images", table => {
+      table.increments();
+      table
+        .integer("property_id")
+        .unsigned()
+        .references("id")
+        .onTable("properties")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table.string("path").notNullable();
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("property_images");
+  }
+}
+
+module.exports = PropertyImageSchema;
