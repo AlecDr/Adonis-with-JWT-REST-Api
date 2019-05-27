aea1e77667ec272fc2f04d1d90906fd91fd51a6e
AlecDr <alecsander_rs@hotmail.com>
2019-05-26 21:05:49 -0300

    adding the react-native app

---
+++ Airbnbclone/App.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+import { View, Text } from "react-native";
+
+export default class App {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
