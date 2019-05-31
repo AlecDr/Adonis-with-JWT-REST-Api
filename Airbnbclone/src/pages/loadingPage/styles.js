da3a59d90ab54245c980a27f4734e15b74d7f59e
AlecDr <alecsander_rs@hotmail.com>
2019-05-30 21:16:50 -0300

    adding the signin page

--- Airbnbclone/src/pages/loadingPage/styles.js
+++ Airbnbclone/src/pages/loadingPage/styles.js
@@ -14,19 +14,15 @@ export const Text = styled.Text`
 `;
 
 export const ButtonText = styled.Text`
-  font-size: 15px;
+  font-size: 14px;
   color: #fff;
 `;
 
 export const AuthenticateButton = styled.TouchableOpacity`
   width: 90%;
-  padding: 20px;
-  font-size: 20px;
+  padding: 16px;
   background-color: #ff7a7d;
   margin: 10px;
-  text-align: center;
-  border-radius: 10px;
+  border-radius: 6px;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
 `;
--- Airbnbclone/src/pages/signIn/index.js
+++ Airbnbclone/src/pages/signIn/index.js
@@ -1,13 +1,82 @@
 import React from "react";
+import { Dimensions } from "react-native";
+
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import Image from "react-native-scalable-image";
+
+import {
+  Button,
+  ButtonText,
+  Container,
+  TextInput,
+  InputContainer,
+  InputLabel
+} from "./styles";
 
 export default class SignIn extends React.Component {
+  state = {
+    error: "",
+    email: "",
+    password: ""
+  };
+
+  handleEmailChange = email => {
+    this.setState({ email });
+  };
+
+  handlePasswordChange = password => {
+    this.setState({ password });
+  };
+
   static navigationOptions = {
     tabBarIcon: <Icon style={{ fontSize: 20 }} name="login" />,
     tabBarColor: "#ff7a7d"
   };
 
   render() {
-    return null;
+    return (
+      <KeyboardAwareScrollView
+        contentContainerStyle={{
+          flexDirection: "column",
+          justifyContent: "center",
+          backgroundColor: "#fff",
+          flexGrow: 1
+        }}
+        style={{ flex: 1 }}
+      >
+        <Container behavior="padding" enabled>
+          <Image
+            width={Dimensions.get("window").width * 0.5}
+            source={require("../../images/logo.png")}
+          />
+          <InputContainer>
+            <InputLabel>Email</InputLabel>
+            <TextInput
+              onChangeText={this.handleEmailChange}
+              value={this.state.email}
+              autoCapitalize={false}
+              autoCorrect={false}
+              placeholder="Email"
+            />
+          </InputContainer>
+          <InputContainer>
+            <InputLabel>Password</InputLabel>
+            <TextInput
+              onChangeText={this.handlePasswordChange}
+              value={this.state.password}
+              autoCapitalize={false}
+              autoCorrect={false}
+              secureTextEntry
+              placeholder="********"
+            />
+          </InputContainer>
+          <Button>
+            <ButtonText>Login</ButtonText>
+          </Button>
+        </Container>
+      </KeyboardAwareScrollView>
+    );
   }
 }
