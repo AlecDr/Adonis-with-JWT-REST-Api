003012bb1edb9bee76ee8e3fa00684cea5845589
AlecDr <alecsander_rs@hotmail.com>
2019-05-31 21:18:11 -0300

    Fixing auth flow

--- api/app/Controllers/Http/AuthController.js
+++ api/app/Controllers/Http/AuthController.js
@@ -5,11 +5,16 @@ class AuthController {
    * @param {Request} {request}
    * @return {JWT Token} token
    */
-  async create({ request, auth }) {
-    const { email, password } = request.all();
-    const token = await auth.attempt(email, password);
+  async create({ request, auth, response }) {
+    try {
+      const { email, password } = request.all();
+      const token = await auth.attempt(email, password);
 
-    return token;
+      return token;
+    } catch (error) {
+      console.log(error);
+      return response.status(401).send({ message: "Email or password wrong!" });
+    }
   }
 }
 
