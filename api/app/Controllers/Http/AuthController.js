9fd5576a90fef8dc719a9206b363df66215342d5
AlecDr <alecsander_rs@hotmail.com>
2019-06-01 23:16:49 -0300

    Changing the server auth responses, 200 for email or password wrong, 201 for token created

--- api/app/Controllers/Http/AuthController.js
+++ api/app/Controllers/Http/AuthController.js
@@ -1,4 +1,5 @@
 "use strict";
+const User = use("App/Models/User");
 
 class AuthController {
   /**
@@ -9,11 +10,16 @@ class AuthController {
     try {
       const { email, password } = request.all();
       const token = await auth.attempt(email, password);
+      const user = await User.query()
+        .where("email", email)
+        .first();
 
-      return token;
+      return response
+        .status(201)
+        .send({ token, user: { email: user.email, name: user.username } });
     } catch (error) {
       console.log(error);
-      return response.status(401).send({ message: "Email or password wrong!" });
+      return response.status(200).send({ message: "Email or password wrong!" });
     }
   }
 }
