8949ec2c7584237b2f823f5a547717b25b66f349
AlecDr <alecsander_rs@hotmail.com>
2019-06-05 20:40:15 -0300

    adding a dumb user page

--- Airbnbclone/src/pages/userPage/index.js
+++ Airbnbclone/src/pages/userPage/index.js
@@ -1,5 +1,14 @@
 import React from "react";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Image from "react-native-scalable-image";
+
+// custom components
+import {
+  Container,
+  UserInfoContainer,
+  UserNameText,
+  UserEmailText
+} from "./styles";
+import { SafeAreaView, Dimensions } from "react-native";
 
 export default class UserPage extends React.Component {
   state = {
@@ -7,9 +16,28 @@ export default class UserPage extends React.Component {
     userData: null
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.loadUserInfo();
+  }
+
+  loadUserInfo = async () => {};
+
+  renderUserInfo = () => {};
 
   render() {
-    return null;
+    return (
+      <SafeAreaView>
+        <Container>
+          <Image
+            width={Dimensions.get("window").width * 0.35}
+            source={require("../../images/default-user.png")}
+          />
+          <UserInfoContainer>
+            <UserNameText>My very long unique name</UserNameText>
+            <UserEmailText>Some@dumbemail.com</UserEmailText>
+          </UserInfoContainer>
+        </Container>
+      </SafeAreaView>
+    );
   }
 }
---
+++ Airbnbclone/src/pages/userPage/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 60px 20px;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const UserInfoContainer = styled.View`
+  margin: 10px 0px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const UserNameText = styled.Text`
+  color: #111;
+  font-size: 16px;
+`;
+
+export const UserEmailText = styled.Text`
+  color: #666;
+  font-size: 12px;
+`;
