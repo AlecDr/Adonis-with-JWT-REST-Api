bc4d8670b32ef5ddc7cd40ec4a90d996bac8f776
AlecDr <alecsander_rs@hotmail.com>
2019-06-06 21:26:38 -0300

    Adding mapbox-gl

--- Airbnbclone/android/app/src/main/java/com/airbnbclone/MainApplication.java
+++ Airbnbclone/android/app/src/main/java/com/airbnbclone/MainApplication.java
@@ -3,6 +3,7 @@ package com.airbnbclone;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.shimmer.RNShimmerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTMGLPackage(),
             new LinearGradientPackage(),
             new RNShimmerPackage(),
             new VectorIconsPackage(),
