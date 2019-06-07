464517691224308113e711abc74b36094d6ec3d6
AlecDr <alecsander_rs@hotmail.com>
2019-06-07 20:29:43 -0300

    Adding the map findme option

--- Airbnbclone/src/pages/MapPage/index.js
+++ Airbnbclone/src/pages/MapPage/index.js
@@ -1,20 +1,60 @@
 import React from "react";
 import Mapbox from "@react-native-mapbox-gl/maps";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 // custom components
-import { Container } from "./styles";
+import { Container, FindMeButton } from "./styles";
 
 export default class MapPage extends React.Component {
+  state = {
+    location: [-56.00663, -28.65408]
+  };
+
   componentWillMount() {
     Mapbox.setAccessToken(
       "pk.eyJ1IjoiYWxlY2RyIiwiYSI6ImNqc3hiOTViaDBrNWU0YW14dDA2YjNvdTYifQ.fwUsz-HyzY4STwp9A61G9Q"
     );
   }
 
+  componentDidMount() {
+    Mapbox.setTelemetryEnabled(false);
+  }
+
+  findMeHandler = async () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        location: [position.coords.longitude, position.coords.latitude]
+      });
+    });
+  };
+
   render() {
     return (
       <Container>
-        <Mapbox.MapView style={{ flex: 1 }} />
+        <Mapbox.MapView
+          animated={true}
+          logoEnabled={false}
+          styleURL={Mapbox.StyleURL.Light}
+          attributionEnabled={false}
+          style={{ flex: 1 }}
+        >
+          <Mapbox.Camera
+            zoomLevel={13}
+            animationMode={"flyTo"}
+            animationDuration={2000}
+            centerCoordinate={this.state.location}
+          />
+          <Mapbox.UserLocation />
+        </Mapbox.MapView>
+
+        <FindMeButton onPress={this.findMeHandler}>
+          <Icon
+            style={{ margin: 0, padding: 0 }}
+            name="my-location"
+            size={30}
+            color="#fff"
+          />
+        </FindMeButton>
       </Container>
     );
   }
--- Airbnbclone/src/pages/MapPage/styles.js
+++ Airbnbclone/src/pages/MapPage/styles.js
@@ -3,3 +3,18 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
 `;
+
+export const FindMeButton = styled.TouchableOpacity`
+  width: 60;
+  height: 60;
+  border-radius: 50;
+  background-color: #880e4f;
+  position: absolute;
+  bottom: 20;
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  align-items: center;
+  right: 20;
+  z-index: 100;
+`;
