406ea3b1fa20ac0e2f7279782d708de17824718a
AlecDr <alecsander_rs@hotmail.com>
2019-06-07 19:20:23 -0300

    Adding the mapbox

--- Airbnbclone/src/pages/MapPage/index.js
+++ Airbnbclone/src/pages/MapPage/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Mapbox from "@react-native-mapbox-gl/maps";
+
+// custom components
+import { Container } from "./styles";
 
 export default class MapPage extends React.Component {
+  componentWillMount() {
+    Mapbox.setAccessToken(
+      "pk.eyJ1IjoiYWxlY2RyIiwiYSI6ImNqc3hiOTViaDBrNWU0YW14dDA2YjNvdTYifQ.fwUsz-HyzY4STwp9A61G9Q"
+    );
+  }
+
   render() {
-    return null;
+    return (
+      <Container>
+        <Mapbox.MapView style={{ flex: 1 }} />
+      </Container>
+    );
   }
 }
