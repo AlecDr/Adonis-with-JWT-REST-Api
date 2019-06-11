758b438805e133c6b8502f1467df7d9a567f4cc4
AlecDr <alecsander_rs@hotmail.com>
2019-06-10 21:05:00 -0300

    Adding the fab to the create property page

--- Airbnbclone/src/pages/addPropertyPage/index.js
+++ Airbnbclone/src/pages/addPropertyPage/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import MapboxGL from "@react-native-mapbox-gl/maps";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 // custom components
 import { Container, MapContainer, FormContainer } from "./styles";
 
@@ -29,6 +32,29 @@ export default class AddPropertyPage extends React.Component {
           </MapboxGL.MapView>
         </MapContainer>
         <FormContainer />
+        <ActionButton
+          renderIcon={() => (
+            <Icon size={30} name="add" style={{ color: "white" }} />
+          )}
+          offsetX={20}
+          offsetY={20}
+          buttonColor="#ff6064"
+        >
+          <ActionButton.Item
+            buttonColor="#880e4f"
+            title="Create property"
+            onPress={() => null}
+          >
+            <Icon size={20} name="check" style={{ color: "white" }} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#4a148c"
+            title="Find me!"
+            onPress={() => null}
+          >
+            <Icon size={20} name="my-location" style={{ color: "white" }} />
+          </ActionButton.Item>
+        </ActionButton>
       </Container>
     );
   }
