d0321c759f066687aad756edd109292235e9f2e0
AlecDr <alecsander_rs@hotmail.com>
2019-06-28 21:42:47 -0300

    Adding user logout

--- Airbnbclone/src/components/CustomDrawerComponent/CustomDrawerComponent.js
+++ Airbnbclone/src/components/CustomDrawerComponent/CustomDrawerComponent.js
@@ -1,7 +1,23 @@
 import React from "react";
-import { Dimensions, SafeAreaView, ScrollView, View } from "react-native";
+import {
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableNativeFeedback
+} from "react-native";
 import { DrawerItems } from "react-navigation";
 import Image from "react-native-scalable-image";
+import Icon from "react-native-vector-icons/SimpleLineIcons";
+import { LogoutContainer, LogoutButton, LogoutButtonText } from "./styles";
+import { SwitchActions } from "react-navigation";
+import { AsyncStorage } from "react-native";
+
+const Logout = async navigation => {
+  await AsyncStorage.multiRemove(["user_token", "user_name", "user_email"]);
+  navigation.dispatch(SwitchActions.jumpTo({ routeName: "AuthStack" }));
+};
 
 const CustomDrawerComponent = props => {
   return (
@@ -16,6 +32,19 @@ const CustomDrawerComponent = props => {
       <ScrollView>
         <DrawerItems {...props} />
       </ScrollView>
+      <LogoutContainer>
+        <TouchableNativeFeedback
+          onPress={() => {
+            Logout(props.navigation);
+          }}
+          background={TouchableNativeFeedback.SelectableBackground()}
+        >
+          <LogoutButton>
+            <Icon name="logout" size={15} color="white" />
+            <LogoutButtonText>Logout</LogoutButtonText>
+          </LogoutButton>
+        </TouchableNativeFeedback>
+      </LogoutContainer>
     </SafeAreaView>
   );
 };
---
+++ Airbnbclone/src/components/CustomDrawerComponent/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const LogoutContainer = styled.View`
+  margin: 1px 0px;
+`;
+
+export const LogoutButton = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+  background-color: red;
+  color: #eee;
+`;
+
+export const LogoutButtonText = styled.Text`
+  margin-left: 40px;
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+`;
