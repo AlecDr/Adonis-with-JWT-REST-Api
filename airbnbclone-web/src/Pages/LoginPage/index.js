0e550c28b18642a401ae6c272d02552e3b7e8b09
AlecDr <alecsander_rs@hotmail.com>
2019-07-03 21:03:30 -0300

    Adding a message and inputs handler on login page

--- airbnbclone-web/src/Pages/LoginPage/index.js
+++ airbnbclone-web/src/Pages/LoginPage/index.js
@@ -7,33 +7,51 @@ import styles from "./styles.module.css";
 export default props => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  let login = async event => {
+    event.preventDefault();
+
+    if (checkInputs()) {
+    } else {
+      setMessage("You should provide an email and password!");
+
+      setTimeout(() => setMessage(""), 3000);
+    }
+  };
+
+  let checkInputs = () => {
+    return email.trim().length > 0 && password.trim().length > 0;
+  };
 
   return (
     <div className={styles.registerCard}>
       <img className={styles.logo} src={logo} alt="" />
       <h2 className={styles.title}>Login</h2>
-
-      <div className={styles.inputGroup}>
-        <label className={styles.label}>Email</label>
-        <input
-          onChange={event => setEmail(event.target.value)}
-          value={email}
-          type="email"
-          className={styles.input}
-        />
-      </div>
-      <div className={styles.inputGroup}>
-        <label className={styles.label}>Password</label>
-        <input
-          onChange={event => setPassword(event.target.value)}
-          value={password}
-          type="password"
-          className={styles.input}
-        />
-      </div>
-      <button className={[styles.button, styles.loginButton].join(" ")}>
-        Login
-      </button>
+      <form onSubmit={login}>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Email</label>
+          <input
+            onChange={event => setEmail(event.target.value)}
+            value={email}
+            type="email"
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label className={styles.label}>Password</label>
+          <input
+            onChange={event => setPassword(event.target.value)}
+            value={password}
+            type="password"
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.alert}>{message}</div>
+        <button className={[styles.button, styles.loginButton].join(" ")}>
+          Login
+        </button>
+      </form>
       <hr className={styles.divider} />
       <Link to="/register">
         <button className={[styles.button, styles.registerButton].join(" ")}>
