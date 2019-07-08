d0970fd524b07cee9e1f50f32297256a6a92e80b
AlecDr <alecsander_rs@hotmail.com>
2019-07-08 18:00:59 -0300

    Changing the mapbox for google maps

--- airbnbclone-web/src/Pages/MapPage/index.js
+++ airbnbclone-web/src/Pages/MapPage/index.js
@@ -1,35 +1,43 @@
 import React, { useEffect } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import ReactMapGL from "react-map-gl";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import styles from "./styles.module.css";
 
 export default props => {
   const [mode, setMode] = React.useState("map");
   const [userLocation, setUserLocation] = React.useState({
-    latitude: 37.7577,
-    longitude: -120.4376,
+    lat: 37.7577,
+    long: -120.4376,
     zoom: 8
   });
 
   let onViewportChange = viewport => {
-    let { latitude, longitude, zoom } = viewport;
+    let { lat, long, zoom } = viewport;
 
     setUserLocation({
-      latitude,
-      longitude,
+      lat,
+      long,
       zoom
     });
   };
 
   return (
     <div className={styles.container}>
-      <ReactMapGL
-        onViewportChange={onViewportChange}
-        width="100%"
-        height="100%"
-        {...userLocation}
-        mapboxApiAccessToken="pk.eyJ1IjoiYWxlY2RyIiwiYSI6ImNqd21xeHlnbDBldHk0OGtlc2E0dmExbDEifQ.6j75oBJ1XdFbgbmFc4W7Fw"
-      />
+      <LoadScript
+        style={{ flex: 1 }}
+        id="script-loader"
+        googleMapsApiKey="AIzaSyAcKaRUoIwEeLg_A5zi9ufUnSGfTnCm9qc"
+      >
+        <GoogleMap
+          mapContainerClassName="App-map"
+          center={{ lat: 52.52047739093263, lng: 13.36653284549709 }}
+          zoom={12}
+          mapContainerStyle={{ flex: 1 }}
+          version="weekly"
+          on
+        />
+      </LoadScript>
     </div>
   );
 };
