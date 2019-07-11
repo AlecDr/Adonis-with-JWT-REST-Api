89806aec422876eed22681bfcbd04c1c3d388889
AlecDr <alecsander_rs@hotmail.com>
2019-07-11 20:26:15 -0300

    Adding a spinner to the modal with custom styles

--- airbnbclone-web/src/components/PropertyModal/index.js
+++ airbnbclone-web/src/components/PropertyModal/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Modal from "react-responsive-modal";
 import styles from "./styles.module.css";
+import { ScaleLoader } from "react-spinners";
 
 export default props => {
   const [property, setProperty] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
 
   let renderContent = () => (loading ? renderSpinner() : <p>Content</p>);
-  let renderSpinner = () => <p>Loading...</p>;
+  let renderSpinner = () => (
+    <div className={styles.spinnerContanainer}>
+      <ScaleLoader height={35} width={4} radius={2} margin="2px" />
+    </div>
+  );
 
   return (
     <Modal
